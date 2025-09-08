cb5947f8f4b5c0e304040210daa275c0939a8930
ertyu007 <gcc40437@gmail.com>
2025-09-08 21:02:50 +0700

    fix: เพิ่ม projects.js และแก้ path

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const { Pool } = require('pg');
-const { projectsData } = require('./data/projects.js');
+const { projectsData } = require('/src/data/projects.js');
 
 const app = express();
 app.use(cors({
